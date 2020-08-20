88cd05db2edf989c744a228288738c70f8eccd1d
Sravya Kaki <sravyakaki92@gmail.com>
2020-08-19 18:28:34 -0700

    Using VNdHStack

---
+++ SwiftUIApp/SwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIApp
+//
+//  Created by Sravya Kaki on 8/17/20.
+//  Copyright © 2020 Sravya Kaki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
