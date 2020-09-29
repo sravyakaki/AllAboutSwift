ff6f3c37153354515270171ff572e32c4471b3bb
Sravya Kaki <sravyakaki92@gmail.com>
2020-09-28 18:40:10 -0700

    Complete Quiz app

--- SwiftUIApp/SwiftUIApp/ContentView.swift
+++ SwiftUIApp/SwiftUIApp/ContentView.swift
@@ -10,9 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-    }
+      
+        Text("Modify me")
+            
+
+        }
 }
+
+        
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
