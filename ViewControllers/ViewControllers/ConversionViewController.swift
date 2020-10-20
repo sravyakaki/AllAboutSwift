ce99eb8aceb87ed64e9d8e6009a14a355e8b3950
Sravya Kaki <sravyakaki92@gmail.com>
2020-10-20 13:27:02 -0700

    Building VC through Interface builders

---
+++ ViewControllers/ViewControllers/ConversionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewControllers
+//
+//  Created by Sravya Kaki on 10/16/20.
+//  Copyright © 2020 Sravya Kaki. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ViewControllers/ViewControllers/MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewController.swift
+//  ViewControllers
+//
+//  Created by Sravya Kaki on 10/19/20.
+//  Copyright © 2020 Sravya Kaki. All rights reserved.
+//
+
+import Foundation
