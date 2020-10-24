3b21b34534702d136db84687541394cd502aab8b
Sravya Kaki <sravyakaki92@gmail.com>
2020-10-24 15:16:05 -0700

    Ch 5 Bronze Challenge: Points of Interest

--- ProgrammaticViews/WorldTrotter/MapViewController.swift
+++ ProgrammaticViews/WorldTrotter/MapViewController.swift
@@ -14,6 +14,7 @@ class MapViewController: UIViewController {
         mapView = MKMapView()
         view = mapView
         
+        //Adding Segmented Control
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.systemBackground
         segmentedControl.selectedSegmentIndex = 0
@@ -30,6 +31,33 @@ class MapViewController: UIViewController {
         topConstraint.isActive = true
         leadingConstraint.isActive = true
         trailingConstraint.isActive = true
+        
+        //Adding POI Label
+        let pointsOfInterest = UILabel()
+        pointsOfInterest.text = "Points of Interest"
+        pointsOfInterest.textAlignment = .center
+        pointsOfInterest.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pointsOfInterest)
+        
+        let topConstraintOfLabel = pointsOfInterest.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 55)
+        let leadingConstraintOfLabel = pointsOfInterest.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        
+        
+        topConstraintOfLabel.isActive = true
+        leadingConstraintOfLabel.isActive = true
+        
+        //Adding POI Switch
+        
+        let pointOfInterestSwitch = UISwitch()
+        pointOfInterestSwitch.addTarget(self, action: #selector(displayPointsOfInterest(_:)), for: .valueChanged)
+        pointOfInterestSwitch.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pointOfInterestSwitch)
+        
+        let topConstraintSwitch = pointOfInterestSwitch.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50)
+        let leadingConstraintSwitch = pointOfInterestSwitch.leadingAnchor.constraint(greaterThanOrEqualToSystemSpacingAfter: pointsOfInterest.leadingAnchor, multiplier: 18)
+        
+        topConstraintSwitch.isActive = true
+        leadingConstraintSwitch.isActive = true
         
         
     }
@@ -52,6 +80,24 @@ class MapViewController: UIViewController {
         default:
             break
         }
+    }
+    
+    @objc func displayPointsOfInterest(_ toggleSwitch: UISwitch) {
+            
+        if toggleSwitch.isOn {
+            /*
+            mapView.pointOfInterestFilter = .includingAll
+            print(mapView.pointOfInterestFilter = .includingAll) */
+            let coordinate = CLLocationCoordinate2D(
+                        latitude: 12.9352, longitude: 77.6244)
+            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+            mapView.pointOfInterestFilter = .some(MKPointOfInterestFilter(including: [.restaurant, .park, .brewery]))
+        } else {
+            mapView.pointOfInterestFilter = .excludingAll
+        }
+
     }
         
 }
