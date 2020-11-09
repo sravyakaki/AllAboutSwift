4653c5f7b0c5448f86b6ed6f13d1f3ebc1117813
Sravya Kaki <sravyakaki92@gmail.com>
2020-11-08 19:52:51 -0800

    Ch6: Silver challenge

--- Ch6TextInputAndDelegates/WorldTrotter/MapViewController.swift
+++ Ch6TextInputAndDelegates/WorldTrotter/MapViewController.swift
@@ -3,11 +3,40 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+   
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//        mapView.delegate = self
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+
+
+//        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+//            locationManager.startUpdatingLocation()
+//        } else {
+//            locationManager.requestWhenInUseAuthorization()
+//        }
         print("MapViewController loaded its view.")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locations = locations.first!
+        let coordinateRegion = MKCoordinateRegion(center: locations.coordinate,latitudinalMeters: 500,longitudinalMeters: 500)
+        mapView.setRegion(coordinateRegion, animated: true)
+        //locationManager.stopUpdatingLocation()
+        
+        
     }
 }
