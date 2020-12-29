09541b9b05b6d74ebd796e3d888929102dd82eb5
Sravya Kaki <sravyakaki92@gmail.com>
2020-12-28 21:09:05 -0800

    Codable changes

---
+++ Ch13Codable/LootLogger/LootLogger/DetailViewController.swift
@@ -0,0 +1,80 @@
+//
+//  DetailViewController.swift
+//  LootLogger
+//
+//  Created by Sravya Kaki on 12/19/20.
+//  Copyright © 2020 Sravya Kaki. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    // The following is an example of a propert observer
+    var item: Item! {
+    didSet{
+        navigationItem.title = item.name
+        }
+        
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        view.endEditing(true)
+        item.name = nameField.text ?? ""
+        item.serialNumber = serialNumberField.text ?? ""
+        
+        if let valueText = valueField.text,
+            let value = numberFormatter.number(from: valueText) {
+            item.valueInDollars = value.intValue
+        } else {
+            item.valueInDollars = 0
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
